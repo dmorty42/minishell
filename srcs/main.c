f78c09bb1bf36af48c4a9960a16026e989442323
Darkside Morty <dmorty@un-l5.kzn.21-school.ru>
2021-12-22 03:26:13 +0300

    v 0.004

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: dmorty <dmorty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 19:49:46 by dmorty            #+#    #+#             */
-/*   Updated: 2021/12/21 20:31:31 by dmorty           ###   ########.fr       */
+/*   Updated: 2021/12/22 01:20:26 by dmorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,12 @@ void	execute_cmd(t_node *data, char **env)
 	pid = fork();
 	if (pid == 0)
 	{
-		while (data->redir_num)
+		if (data->r.l_num || data->r.r_num)
 		{
-			dup2(data->red->fd, data->red->dup_num);
-			data->redir_num--;
-			data->red = data->red->next;
+			if (data->r.l_num)
+				dup2(data->r.l_fd, 0);
+			if (data->r.r_num)
+				dup2(data->r.r_fd, 1);
 		}
 		if (isItBuildin(data))
 		{
@@ -50,19 +51,12 @@ void	init(t_node *data)
 	data->cmd = NULL;
 	data->cmd_num = 1;
 	data->redir_num = 0;
-	data->red = NULL;
-}
-
-void	red_clear(t_node *data)
-{
-	t_red	*temp;
-
-	while (data->red)
-	{
-		temp = data->red->next;
-		data->red = NULL;
-		data->red = temp;
-	}
+	data->r.r_num = 0;
+	data->r.l_num = 0;
+	data->r.r_fd = 0;
+	data->r.l_fd = 0;
+	data->r.x_fd = 0;
+	data->r.x_num = 0;
 }
 
 void	cycle_clean(t_node *data)
@@ -77,9 +71,16 @@ void	cycle_clean(t_node *data)
 		free(data->cmd);
 		data->cmd = NULL;
 	}
-	if (data->redir_num)
-		red_clear(data);
-	data->redir_num = 0;
+	if (data->r.l_num)
+	{
+		close(data->r.l_fd);
+		data->r.l_num = 0;
+	}
+	if (data->r.r_num)
+	{
+		close(data->r.r_fd);
+		data->r.r_num = 0;
+	}
 }
 
 int	main(int argc, char **argv, char **env)
@@ -90,24 +91,28 @@ int	main(int argc, char **argv, char **env)
 
 	i = 0;
 	argv = NULL;
+	line = NULL;
 	data = (t_node *)malloc(sizeof(t_node));
 	data->env_lst = parse_env(data, env);
 	init(data);
 	while (argc)
 	{
 		line = readline("minishell: ");
-		add_history(line);
-		data->cmd_num = 1;
-		i = 0;
-		check_semicolon(line, data);
-		while (data->cmd_num > 0)
+		if (ft_strlen(line) > 0)
 		{
-			data->arg[i] = space_prepare(data->arg[i]);
-			parser(data->arg[i], data->env_lst, data);
-			execute_cmd(data, env);
-			cycle_clean(data);
-			i++;
-			data->cmd_num--;
+			add_history(line);
+			data->cmd_num = 1;
+			i = 0;
+			check_semicolon(line, data);
+			while (data->cmd_num > 0)
+			{
+				data->arg[i] = space_prepare(data->arg[i]);
+				parser(data->arg[i], data->env_lst, data);
+				execute_cmd(data, env);
+				cycle_clean(data);
+				i++;
+				data->cmd_num--;
+			}
 		}
 	}
 }
